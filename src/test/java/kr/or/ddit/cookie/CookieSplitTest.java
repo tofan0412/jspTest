b2d35f526cf4c7bba7ac0d07ebb6c3d1d3ac53c7
whc <tofan12312@gmail.com>
2020-10-06 13:33:26 +0900

    cookie 예제

--- src/test/java/kr/or/ddit/cookie/CookieSplitTest.java
+++ src/test/java/kr/or/ddit/cookie/CookieSplitTest.java
@@ -15,16 +15,17 @@ public class CookieSplitTest {
 		CookieSplit cookieSplit = new CookieSplit();
 		
 		/***When***/
-		// value를 반환한다. 입력값이 존재하는 cookie일 경우 해당 cookie의 value를 반환하고
+		// getCookieValue(String str) : value를 반환한다. 
+		// 입력값이 존재하는 cookie일 경우 해당 cookie의 value를 반환하고
 		// 존재하지 않는 경우 공백 ""을 반환한다.
 		String test1 = cookieSplit.getCookieValue("USERNM");
 		String test2 = cookieSplit.getCookieValue("TEST");
 		String test3 = cookieSplit.getCookieValue("XXXX");
+		
 		/***Then***/
-		// 동일하면 통과한다. 
+		// 기댓값과 동일하면 통과한다. 
 		assertEquals("brown", test1);
 		assertEquals("T", test2);
 		assertEquals("", test3);
-		
 	}
 }
